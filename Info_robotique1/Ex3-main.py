86648e78d9cfc3cba547a938542757daedf06903
Emmiche <po03kyi@eduvaud.ch>
2025-08-26 08:57:25 +0200

    Ajouter un ex

---
+++ Info_robotique1/Ex3-main.py
@@ -0,0 +1,33 @@
+"""
+Emma Favre
+26 août 2025
+
+
+
+"""
+
+# Imports go at the top
+from microbit import *
+import speech
+import music
+import neopixel
+np = neopixel.NeoPixel(pin8, 60)
+
+# On commence avec le programme
+np = neopixel.NeoPixel(pin8, 60)
+np[0] = (50, 0, 0)
+np[1] = (0, 50, 0)
+np[2] = (0, 0, 50)
+np.show()
+
+            
+            
+        
+
+
+
+        
+    
+
+
+    
