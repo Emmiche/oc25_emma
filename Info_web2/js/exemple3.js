377d009ac3b29cfdd7e60bb1e9891b476e5eae0f
Emmiche <po03kyi@eduvaud.ch>
2025-11-20 15:15:38 +0100

    Avancement web

--- Info_web2/js/exemple3.js
+++ Info_web2/js/exemple3.js
@@ -10,18 +10,36 @@ ctx_statut.fillRect(0, 0, 300, 300);
 const dessin = document.getElementById("dessin");
 const context = dessin.getContext("2d");
 
-context.fillStyle = "black"
-context.fillRect(0, 0, 200, 100)
-context.fillRect(200, 200, 100, 100)
 
-for (var i = 0; i < 5; i++) {
-    for (var j = 0; j < 5; j++) {
-        if ((i + j) % 2 == 0) {
-            context.fillStyle = "black";
-        } else {
-            context.fillStyle = "white";
-        }
+context.beginPath();
+context.strokeStyle = "red"; // changer couleur trait
+context.moveTo(250, 100);
+context.lineTo(100, 250); //comme forward avec python
+context.lineTo(250, 400);
+context.lineTo(400, 250);
+context.closePath(); // ferme "chemin" pour nous
+context.fillStyle = "red";
+context.fill();
+context.stroke(); // appeler fonction strok pour dessiner 
 
-        context.fillRect(100 * i, 100 * j, 100, 100);
-    }
-}
+
+context.beginPath();
+context.arc(250, 250, 50, 0, 2 * Math.PI); // position, position, rayon?, angle? (en radiant)
+context.fillStyle = "black";
+context.fill();
+
+// context.fillStyle = "black"
+// context.fillRect(0, 0, 200, 100)
+// context.fillRect(200, 200, 100, 100)
+
+// for (var i = 0; i < 5; i++) {
+//     for (var j = 0; j < 5; j++) {
+//         if ((i + j) % 2 == 0) {
+//             context.fillStyle = "black";
+//         } else {
+//             context.fillStyle = "white";
+//         }
+
+//         context.fillRect(100 * i, 100 * j, 100, 100);
+//     }
+// }
