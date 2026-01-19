03c138635868fc42ff7af0ee8506dfa34341c882
Emmiche <po03kyi@eduvaud.ch>
2026-01-19 09:54:18 +0100

    Avancement Dodge the creeps

--- 4_jeu/tutoriel2d/main.gd
+++ 4_jeu/tutoriel2d/main.gd
@@ -3,8 +3,8 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func game_over() -> void:
-	pass # Replace with function body.
+# func game_over() -> void:
+	# pass # Replace with function body.
 
 func game_over():
 	$ScoreTimer.stop()
@@ -14,3 +14,72 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+
+# func _on_mob_timer_timeout() -> void:
+	# pass # Replace with function body.
+
+# func _on_score_timer_timeout() -> void:
+	# pass # Replace with function body.
+
+# func _on_start_timer_timeout() -> void:
+	# pass # Replace with function body.
+
+func _on_score_timer_timeout():
+	score += 1
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's position to the random location.
+	mob.position = mob_spawn_location.position
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+
+func _ready():
+	pass
+
+extends CanvasLayer
+
+# Notifies `Main` node that the button has been pressed
+signal 
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
