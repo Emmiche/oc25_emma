cb35b29c78bc5b05018d3c52d954b8a075ae136e
Emmiche <po03kyi@eduvaud.ch>
2026-01-19 20:18:58 +0100

    Avancement

--- 4_jeu/tutoriel2d/hud.gd
+++ 4_jeu/tutoriel2d/hud.gd
@@ -23,9 +23,9 @@ func show_game_over():
 func update_score(score):
 	$ScoreLabel.text = str(score)
 	
-# func _on_start_button_pressed():
-	# $StartButton.hide()
-	# start_game.emit()
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
 
-# func _on_message_timer_timeout():
-	# $Message.hide()
+func _on_message_timer_timeout():
+	$Message.hide()
